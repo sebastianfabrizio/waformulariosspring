00db4cb17fae91ec137a17228ccc912043d7ba40
sebastianfabrizio <sebasotaradiaz@gmail.com>
2025-03-16 21:47:07 -0500

    controlador y modelo imc

---
+++ src/main/java/pe/edu/cibertec/waformularios/controller/ImcController.java
@@ -0,0 +1,52 @@
+package pe.edu.cibertec.waformularios.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.cibertec.waformularios.model.ImcModel;
+
+@Controller
+public class ImcController {
+
+
+    @GetMapping("/imc")
+    public String inicioImc(Model model){
+        model.addAttribute("imcModel", new ImcModel());
+        model.addAttribute("mostrarMensaje", false);
+        return "imc";
+    }
+
+    @PostMapping("/imc")
+    public String calcularImc(@ModelAttribute("imcModel") ImcModel imcModel, Model model){
+        double tallam = imcModel.getTalla() / 100;
+        double valorImc = imcModel.getPeso() / (tallam * tallam);
+        String diagnostico = "";
+
+        if(valorImc <= 18.5){
+            diagnostico = "Por debajo del peso";
+        }else if(valorImc <=25){
+            diagnostico = "con peso normal";
+        }
+        else if(valorImc <= 30){
+            diagnostico = "Con sobrepeso";
+        }
+        else if(valorImc <=35){
+            diagnostico = "con obesidad leve";
+        }
+        else if(valorImc <= 39){
+            diagnostico = "con obesidad media";
+        }
+        else{
+            diagnostico = "con obesidad mórbida";
+        }
+        model.addAttribute("mostrarMensaje", true);
+        model.addAttribute("resultado", "Su valor imc es: " +
+                String.format("%.2f", valorImc) + ", usted se encuentra "+ diagnostico);
+        return "imc";
+    }
+
+
+}
---
+++ src/main/java/pe/edu/cibertec/waformularios/model/ImcModel.java
@@ -0,0 +1,12 @@
+package pe.edu.cibertec.waformularios.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImcModel {
+    private double talla;
+    private double peso;
+}
