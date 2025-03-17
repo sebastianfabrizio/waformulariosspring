c365416cc6f3eaecf9cf9bc688eb2a02a2983bd0
sebastianfabrizio <sebasotaradiaz@gmail.com>
2025-03-16 20:54:49 -0500

    login controller y diseño terminado

--- src/main/java/pe/edu/cibertec/waformularios/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/waformularios/controller/LoginController.java
@@ -3,6 +3,8 @@ package pe.edu.cibertec.waformularios.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pe.edu.cibertec.waformularios.model.LoginModel;
 
 @Controller
@@ -14,4 +16,15 @@ public class LoginController {
         model.addAttribute("loginModel", new LoginModel());
         return "login";
     }
+
+    @PostMapping("/login")
+    public String validarLogin(@ModelAttribute("loginModel") LoginModel loginModel, Model model){
+
+        if(loginModel.getUsuario().equals("sebastian") &&
+                loginModel.getPassword().equals("123")) {
+            return "home";
+        }
+        return "login";
+    }
+
 }
