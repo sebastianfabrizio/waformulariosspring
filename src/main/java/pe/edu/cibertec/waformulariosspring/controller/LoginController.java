6f829555f7dac80c83f929817729e505f0efaa28
sebastianfabrizio <sebasotaradiaz@gmail.com>
2025-08-19 12:26:58 -0500

    Modelo agregado

--- src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
@@ -2,13 +2,16 @@ package pe.edu.cibertec.waformulariosspring.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pe.edu.cibertec.waformulariosspring.model.LoginModel;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String Login(){
+    public String Login(Model model){
+        model.addAttribute("loginModel", new LoginModel());
         return "login";
     }
 }
--- src/main/java/pe/edu/cibertec/waformulariosspring/model/LoginModel.java
+++ src/main/java/pe/edu/cibertec/waformulariosspring/model/LoginModel.java
@@ -1,6 +1,14 @@
 package pe.edu.cibertec.waformulariosspring.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginModel {
 
+    private String usuario;
+    private String password;
 
 }
